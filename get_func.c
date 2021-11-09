e703414a40d177e9e3badc755677bbb3627412a7
Luis Gonzales <Luis Gonzales>
2021-11-09 00:17:00 +0000

    Task 1

---
+++ get_func.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *get_func - function to find a function pointer associated with specifier
+ *@a: specifier to find the correct function
+ *
+ *Return: function pointer
+ */
+
+int (*get_func(const char a))(va_list)
+{
+unsigned int m = 0;
+
+format_t structs_pr[] = {
+{'c', print_c},
+{'s', print_s},
+{'%', print_percent},
+{'i', print_integer},
+{'d', print_decimal},
+{'b', print_binary},
+{'u', print_unint},
+{'o', print_octal},
+{'R', rot_13},
+{'\0', NULL}
+};
+
+
+for (; structs_pr[m].spc; m++)
+{
+if (a == structs_pr[m].spc)
+return (structs_pr[m].f);
+}
+
+return (NULL);
+}
